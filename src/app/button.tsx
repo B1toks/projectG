ab397a542b8a0fe355d8c27f0567e9dcc16827ff
Alex <megaoh478@gmail.com>
2025-04-11 18:44:16 +0300

    start build 0.1.0 (kaif test)

---
+++ src/app/button.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button"
+
+export default function App() {
+  return (
+    <div className="flex justify-center">
+      <Button className="hover:bg-white hover:text-black w-1/3 sm:w-1/6 lg:w-1/4">Click me</Button>
+    </div>
+  )
+}
